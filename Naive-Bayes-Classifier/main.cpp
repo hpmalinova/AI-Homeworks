0099756ac8469ff284634ca3d27272dafacd80e5
hpmalinova <hrisi.pm@abv.bg>
2020-12-26 13:32:00 +0200

    Add data and fix bugs

--- Naive-Bayes-Classifier/main.cpp
+++ Naive-Bayes-Classifier/main.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <unordered_map>
 #include <fstream>
+#include <algorithm>    /* std::shuffle */
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+#include <cmath>
+#include <string>
 
 using std::string;
 using std::vector;
@@ -18,64 +23,66 @@ const short REPUBLICAN_LAST_ATTRIBUTE = 41;
 
 const char YEA = 'y';
 const char NAY = 'n';
+const char MISSING_VALUE = '?';
 
 const short NUMBER_OF_SETS = 10;
-const std::unordered_map<short, string> sets{{1,  "../votes1.txt"},
-                                             {2,  "../votes2.txt"},
-                                             {3,  "../votes3.txt"},
-                                             {4,  "../votes4.txt"},
-                                             {5,  "../votes5.txt"},
-                                             {6,  "../votes6.txt"},
-                                             {7,  "../votes7.txt"},
-                                             {8,  "../votes8.txt"},
-                                             {9,  "../votes9.txt"},
-                                             {10, "../votes10.txt"}};
+const string fileName = "../allVotes.data";
 
 const short NUMBER_OF_ATTRIBUTES = 16;
 
 const short int INVALID = -1;
 
 
+static vector<string> getFileLines() {
+    string line;
+    vector<string> fileLines;
+    std::ifstream myfile(fileName);
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            fileLines.push_back(line);
+        }
+        myfile.close();
+    } else
+        cout << "Unable to open file " << fileName << "\n";
+    std::random_shuffle(fileLines.begin(), fileLines.end());
+    return fileLines;
+}
+
 // One for each Attribute and for each Class
 class AnswerCount {
 private:
     int yeaCount;
     int nayCount;
+    int missingCount;
 
 public:
-    AnswerCount() : yeaCount(0), nayCount(0) {}
+    AnswerCount() : yeaCount(1), nayCount(1), missingCount(1) {}
 
     void addOneVote(char answer) {
         if (answer == YEA) {
             yeaCount++;
-        }
-        else if (answer == NAY) {
+        } else if (answer == NAY) {
             nayCount++;
+        } else if (answer == MISSING_VALUE) {
+            missingCount++;
         }
     }
 
     [[nodiscard]] double getProbability(char answer) const {
         if (answer == YEA) {
-            return (double) yeaCount / (yeaCount + nayCount);
+            return (double) yeaCount / (yeaCount + nayCount + missingCount - 3);
         } else if (answer == NAY) {
-            return (double) nayCount / (yeaCount + nayCount);
-        }
-        return -1;
-    }
-
-    [[nodiscard]] int getVotesCount(char answer) const {
-        if (answer == YEA) {
-            return yeaCount;
-        }
-        else if (answer == NAY) {
-            return nayCount;
+            return (double) nayCount / (yeaCount + nayCount + missingCount - 3);
+        } else if (answer == MISSING_VALUE) {
+            return (double) missingCount / (yeaCount + nayCount + missingCount - 3);
         }
         return INVALID;
     }
 
     void clearAnswers() {
-        yeaCount = 0;
-        nayCount = 0;
+        yeaCount = 1;
+        nayCount = 1;
+        missingCount = 1;
     }
 };
 
@@ -85,7 +92,7 @@ private:
     std::unordered_map<short, AnswerCount> votes;
 
 public:
-    explicit DataSet() { // TODO votes(16)
+    DataSet() {
         for (int i = 1; i <= NUMBER_OF_ATTRIBUTES; i++) {
             votes.insert({i, AnswerCount()});
         }
@@ -95,21 +102,19 @@ public:
         votes[attribute].addOneVote(answer);
     }
 
-    int getVotesCount(short attribute, char answer){
-        return votes[attribute].getVotesCount(answer);
-    }
-
     double getProbability(short attribute, char answer) {
         if (answer == YEA) {
             return votes[attribute].getProbability(YEA);
         } else if (answer == NAY) {
             return votes[attribute].getProbability(NAY);
+        } else if (answer == MISSING_VALUE) {
+            return votes[attribute].getProbability(MISSING_VALUE);
         }
-        return -1;
+        return INVALID;
     }
 
     void clearVotes() {
-        for (short i = 1; i<= NUMBER_OF_ATTRIBUTES;i++) {
+        for (short i = 1; i <= NUMBER_OF_ATTRIBUTES; i++) {
             votes.at(i).clearAnswers();
         }
     }
@@ -123,104 +128,99 @@ private:
     int republicansCount;
 
 public:
-    void readTrainingSets(const vector<string> &trainingSets) {
-        string line;
-        for (auto &set : trainingSets) {
-            std::ifstream myfile(set);
-            short attr = 1;
-            if (myfile.is_open()) {
-                while (getline(myfile, line)) // "republican,n,y,n,y,y,y,n,n,n,y,n,y,y,y,n,y"
-                {
-                    if (line[0] == REPUBLICAN) {
-                        for (int i = REPUBLICAN_FIRST_ATTRIBUTE; i <= REPUBLICAN_LAST_ATTRIBUTE; i = i + 2) {
-                            republicansVotes.addVote(attr++, line[i]);
-                        }
-                        republicansCount++;
-                    } else if (line[0] == DEMOCRAT) {
-                        for (int i = DEMOCRAT_FIRST_ATTRIBUTE; i <= DEMOCRAT_LAST_ATTRIBUTE; i = i + 2) {
-                            democratsVotes.addVote(attr++, line[i]);
-                        }
-                        democratsCount++;
-                    }
-                    attr = 1;
+    void readTrainingSets(const vector<string> &trainingSet) {
+        short attr;
+
+        // "republican,n,y,n,y,y,y,n,n,n,y,n,y,y,y,n,y"
+        for (auto &line : trainingSet) {
+            attr = 1;
+
+            if (line[0] == REPUBLICAN) {
+                for (int i = REPUBLICAN_FIRST_ATTRIBUTE; i <= REPUBLICAN_LAST_ATTRIBUTE; i += 2) {
+                    republicansVotes.addVote(attr, line[i]);
+                    attr++;
                 }
-                myfile.close();
-            } else
-                cout << "Unable to open file " << set << "\n";
+                republicansCount++;
+            } else if (line[0] == DEMOCRAT) {
+                for (int i = DEMOCRAT_FIRST_ATTRIBUTE; i <= DEMOCRAT_LAST_ATTRIBUTE; i += 2) {
+                    democratsVotes.addVote(attr, line[i]);
+                    attr++;
+                }
+                democratsCount++;
+            }
         }
     }
 
-    double getAccuracyOfValidationSet(const string &validationSet) {
-        string line;
-        std::ifstream myfile(validationSet);
-
-        double democratsProbability = (double) democratsCount / (democratsCount + republicansCount);
-        double republicansProbability = (double) republicansCount / (democratsCount + republicansCount);
-
-        short attr;
+    double getAccuracyOfValidationSet(const vector<string> &validationSet) {
+        double democratsProbability = std::log((double) democratsCount / (double) (democratsCount + republicansCount));
+        double republicansProbability = std::log(
+                (double) republicansCount / (double) (democratsCount + republicansCount));
 
         int correctAnswers = 0;
         int allAnswers = 0;
 
-        if (myfile.is_open()) {
-            while (getline(myfile, line)) // "republican,n,y,n,y,y,y,n,n,n,y,n,y,y,y,n,y"
-            {
-                attr = 1;
+        short attr;
 
-                if (line[0] == REPUBLICAN) {
-                    for (int i = REPUBLICAN_FIRST_ATTRIBUTE; i <= REPUBLICAN_LAST_ATTRIBUTE; i = i + 2) {
-                        republicansProbability *= republicansVotes.getProbability(attr, line[i]);
-                        democratsProbability *= democratsVotes.getProbability(attr, line[i]);
-                        attr++;
-                    }
-                } else if (line[0] == DEMOCRAT) {
-                    for (int i = DEMOCRAT_FIRST_ATTRIBUTE; i <= DEMOCRAT_LAST_ATTRIBUTE; i = i + 2) {
-                        republicansProbability *= republicansVotes.getProbability(attr, line[i]);
-                        democratsProbability *= democratsVotes.getProbability(attr, line[i]);
-                        attr++;
-                    }
-                }
+        for (auto &line : validationSet) {
+            attr = 1;
 
-                if ((republicansProbability > democratsProbability && line[0] == REPUBLICAN) ||
-                    (democratsProbability > republicansProbability && line[0] == DEMOCRAT)) {
-                    correctAnswers++;
-                }
-                allAnswers++;
+            int firstIndex = 0;
+            int lastIndex = 0;
+
+            if (line[0] == REPUBLICAN) {
+                firstIndex = REPUBLICAN_FIRST_ATTRIBUTE;
+                lastIndex = REPUBLICAN_LAST_ATTRIBUTE;
+            } else if (line[0] == DEMOCRAT) {
+                firstIndex = DEMOCRAT_FIRST_ATTRIBUTE;
+                lastIndex = DEMOCRAT_LAST_ATTRIBUTE;
             }
-            myfile.close();
-        } else
-            cout << "Unable to open file " << validationSet << "\n";
 
-        return (double) correctAnswers / allAnswers;
+            double beADemocratProbability = democratsProbability;
+            double beARepublicanProbability = republicansProbability;
+
+            for (int i = firstIndex; i <= lastIndex; i += 2) {
+                beARepublicanProbability += std::log(republicansVotes.getProbability(attr, line[i]));
+                beADemocratProbability += std::log(democratsVotes.getProbability(attr, line[i]));
+                attr++;
+            }
+
+            if (((beARepublicanProbability - beADemocratProbability > 0.00000000000001) && line[0] == REPUBLICAN) ||
+                ((beADemocratProbability - beARepublicanProbability > 0.00000000000001) && line[0] == DEMOCRAT)) {
+                correctAnswers++;
+            }
+            allAnswers++;
+        }
+
+        return (double) correctAnswers / (double) allAnswers; // Accuracy
     }
 
-    NaiveBayesClassifier() : democratsCount(0), republicansCount(0) { // TODO
+    NaiveBayesClassifier() : democratsCount(0), republicansCount(0) {
         democratsVotes = DataSet();
         republicansVotes = DataSet();
     }
 
-    void crossValidation() { // TODO add shuffle
-        double allAccuracies = 0;
+    void crossValidation() {
         double accuracy;
+        double allAccuracies = 0;
+        vector<string> fileLines = getFileLines();
 
-        for (int toValidate = 1; toValidate <= NUMBER_OF_SETS; toValidate++) {
-            string validationSet;
-            vector<string> trainingSets;
+        int linesSize = fileLines.size();
+        int step = linesSize / NUMBER_OF_SETS;
 
-            for (int i = 1; i <= NUMBER_OF_SETS; i++) {
-                if (i != toValidate) {
-                    trainingSets.push_back(sets.at(i));
-                } else {
-                    validationSet = sets.at(i);
-                }
-            }
-            readTrainingSets(trainingSets);
+        for (int toValidate = 0; toValidate < linesSize; toValidate += step) {
+            vector<string> trainingSet(fileLines.begin(), fileLines.begin() + toValidate);
+            vector<string> validationSet(fileLines.begin() + toValidate, fileLines.begin() + toValidate + step);
+            vector<string> endOfTrainingSets(fileLines.begin() + step + toValidate, fileLines.end());
+            trainingSet.insert(trainingSet.end(), endOfTrainingSets.begin(), endOfTrainingSets.end());
+
+            readTrainingSets(trainingSet);
             accuracy = getAccuracyOfValidationSet(validationSet);
             allAccuracies += accuracy;
             cout << "Accuracy: " << accuracy << "\n";
+
             clearVotes();
         }
-        cout << "Average accuracy: " << allAccuracies / NUMBER_OF_SETS << "\n";
+        cout << "\nAverage accuracy: " << allAccuracies / NUMBER_OF_SETS << "\n";
     }
 
     void clearVotes() {
@@ -232,7 +232,10 @@ public:
 };
 
 int main() {
+    srand(time(nullptr));
     NaiveBayesClassifier nbc = NaiveBayesClassifier();
+    cout << "\n";
     nbc.crossValidation();
+
     return 0;
 }
