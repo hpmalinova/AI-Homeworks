26ee0c341e5766e9149b78a6ced000c3ee0d7dbf
hpmalinova <hrisi.pm@abv.bg>
2020-12-30 18:27:28 +0200

    Fix bug in build tree and fix Attribute-to-values map

--- Decision-Tree/main.cpp
+++ Decision-Tree/main.cpp
@@ -5,6 +5,10 @@
 #include <utility>
 #include <vector>
 #include <cmath>
+#include <algorithm>    /* std::shuffle */
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+
 //#include "attributes.cpp"
 
 using std::string;
@@ -13,35 +17,33 @@ using std::cout;
 
 //Class: no-recurrence-events, recurrence-events
 
-static const string RECURRENCE = "recurrence-events";
-static const string NO_RECURRENCE = "no-recurrence-events";
-static const string TIE = "tie";
-static const string MISSING_VALUE = "?";
-static const char INVALID = -1;
+const string RECURRENCE = "recurrence-events";
+const string NO_RECURRENCE = "no-recurrence-events";
+const string MISSING_VALUE = "?";
+const short NUMBER_OF_SETS = 11;
+const int K = 10;
 
+const std::unordered_map<string, vector<string>> ATTRIBUTE_TO_VALUES{{"age",        {"10-19",   "20-29",    "30-39",    "40-49",     "50-59",   "60-69", "70-79", "80-89", "90-99"}},
+                                                               {"menopause",  {"lt40",    "ge40",     "premeno"}},
+                                                               {"tumorSize",  {"0-4",     "5-9",      "10-14",    "15-19",     "20-24",   "25-29", "30-34", "35-39", "40-44",
+                                                                                                                                                                              "45-49", "50-54", "55-59"}},
+                                                               {"invNodes",   {"0-2",     "3-5",      "6-8",      "9-11",      "12-14",   "15-17", "18-20", "21-23", "24-26",
+                                                                                                                                                                              "27-29", "30-32", "33-35", "36-39"}},
+                                                               {"nodeCaps",   {"yes",     "no", MISSING_VALUE}},
+                                                               {"degMalig",   {"1",       "2",        "3"}},
+                                                               {"breast",     {"left",    "right"}},
+                                                               {"breastQuad", {"left_up", "left_low", "right_up", "right_low", "central", MISSING_VALUE}},
+                                                               {"irradiat",   {"yes",     "no"}}};
 
-static const string fileName = "../breast-cancer.data";
-std::unordered_map<string, vector<string>> ATTRIBUTES{{"age",        {"10-19",   "20-29",    "30-39",    "40-49",     "50-59",   "60-69", "70-79", "80-89", "90-99", MISSING_VALUE}},
-                                                      {"menopause",  {"lt40",    "ge40",     "premeno",  MISSING_VALUE}},
-                                                      {"tumorSize",  {"0-4",     "5-9",      "10-14",    "15-19",     "20-24",   "25-29", "30-34", "35-39", "40-44",
-                                                                                                                                                                     "45-49", "50-54", "55-59", MISSING_VALUE}},
-                                                      {"invNodes",   {"0-2",     "3-5",      "6-8",      "9-11",      "12-14",   "15-17", "18-20", "21-23", "24-26",
-                                                                                                                                                                     "27-29", "30-32", "33-35", "36-39", MISSING_VALUE}},
-                                                      {"nodeCaps",   {"yes",     "no",       MISSING_VALUE}},
-                                                      {"degMalig",   {"1",       "2",        "3",        MISSING_VALUE}},
-                                                      {"breast",     {"left",    "right",    MISSING_VALUE}},
-                                                      {"breastQuad", {"left-up", "left-low", "right-up", "right-low", "central", MISSING_VALUE}},
-                                                      {"irradiat",   {"yes",     "no",       MISSING_VALUE}}};
-
-std::unordered_map<string, short> ATTRIBUTES_POSITION{{"age",        1},
-                                                      {"menopause",  2},
-                                                      {"tumorSize",  3},
-                                                      {"invNodes",   4},
-                                                      {"nodeCaps",   5},
-                                                      {"degMalig",   6},
-                                                      {"breast",     7},
-                                                      {"breastQuad", 8},
-                                                      {"irradiat",   9}};
+std::unordered_map<string, short> ATTRIBUTE_TO_POSITION{{"age",        1},
+                                                        {"menopause",  2},
+                                                        {"tumorSize",  3},
+                                                        {"invNodes",   4},
+                                                        {"nodeCaps",   5},
+                                                        {"degMalig",   6},
+                                                        {"breast",     7},
+                                                        {"breastQuad", 8},
+                                                        {"irradiat",   9}};
 
 class AnswerCount {
 private:
@@ -59,9 +61,30 @@ public:
     }
 
     [[nodiscard]] double getEntropy() const {
+        if (recurrenceCount == 0 || noRecurrenceCount == 0) {
+            return 1;
+        }
         double recurrenceProb = (double) recurrenceCount / (recurrenceCount + noRecurrenceCount);
         double noRecurrenceProb = (double) noRecurrenceCount / (recurrenceCount + noRecurrenceCount);
-        return -recurrenceProb * log2(recurrenceProb) - noRecurrenceProb * log2(noRecurrenceProb);
+        return ((-(recurrenceProb * log2(recurrenceProb))) - (noRecurrenceProb * log2(noRecurrenceProb)));
+/*
+        if (recurrenceCount+noRecurrenceCount == 0) {
+            return 1;
+        }
+        double recurrenceProb = (double) recurrenceCount / (recurrenceCount + noRecurrenceCount);
+        double noRecurrenceProb = (double) noRecurrenceCount / (recurrenceCount + noRecurrenceCount);
+
+        double logRec = log2(recurrenceProb);
+        double logNoRec = log2(noRecurrenceProb);
+
+
+        if (std::isnan(logRec)) {
+            logRec = 0;
+        }
+        if (std::isnan(logNoRec)) {
+            logNoRec = 0;
+        }
+        return ((-(recurrenceProb * logRec)) - (noRecurrenceProb * logNoRec));*/
     }
 
     [[nodiscard]] int getCount() const { return recurrenceCount + noRecurrenceCount; }
@@ -80,8 +103,9 @@ private:
     // Ex: "10-19" -> {5 Recurrence, 3 NoRecurrence}
     std::unordered_map<string, AnswerCount> data;
 public:
-    explicit Attribute(const string &attributeName) : attributeName(attributeName) {
-        for (auto &value : ATTRIBUTES[attributeName]) {
+    explicit Attribute(string attributeName) : attributeName(attributeName) {
+        vector<string> values = ATTRIBUTE_TO_VALUES.at(attributeName);//ATTRIBUTE_TO_VALUES[attributeName];
+        for (auto &value : values) {
             data.insert({value, AnswerCount()});
         }
     }
@@ -90,7 +114,9 @@ public:
         double entropy = 0;
         // Ex: value.first == "10-19", "20-29", ..
         for (auto &value: data) {
-            entropy += getProbability(value.first) * value.second.getEntropy();
+            double probability = getProbability(value.first);
+            double entropy2 = value.second.getEntropy();
+            entropy += probability * entropy2;
         }
         return entropy;
     }
@@ -108,7 +134,7 @@ public:
     }
 
     void fill(vector<string> info) {
-        string toAdd = info[ATTRIBUTES_POSITION[attributeName]]; // "10-19"
+        string toAdd = info[ATTRIBUTE_TO_POSITION[attributeName]]; // "10-19"
         data[toAdd].add(info[0]); // Ex: info[0] == Recurrence/ NoRecurrence
     }
 
@@ -125,13 +151,12 @@ class DecisionTreeNode {
 private:
     vector<Attribute> attributes;
     DecisionTreeNode *parent;
-//    vector<DecisionTreeNode *> children; // TODO node?
     std::unordered_map<string, DecisionTreeNode *> children;
     vector<string> withoutAttributes; // TODO ??? exist
     int recurrenceCount;
     int allAnswers;
     string bestAttribute;
-    vector<vector<string>> info;
+//    string targetClass;
 
     bool contains(const string &attribute) { // TODO RENAME
         for (auto &without : withoutAttributes) {
@@ -143,15 +168,19 @@ private:
     }
 
 public:
-    // TODO check to see if constructor works :D
+    // TODO
     explicit DecisionTreeNode(vector<string> withoutAttributes = {},
                               DecisionTreeNode *parent = nullptr) : parent(parent), recurrenceCount(0), allAnswers(0),
-                                                                    info(info),
                                                                     withoutAttributes(std::move(withoutAttributes)) {
     }
 
+    DecisionTreeNode *getChild(const string &value) {
+        return children.at(value);
+    }
+
     void processData(const vector<vector<string>> &info) {
-        for (auto &attribute : ATTRIBUTES) {
+//        auto a = ATTRIBUTE_TO_VALUES;
+        for (auto &attribute : ATTRIBUTE_TO_VALUES) {
             if (!contains(attribute.first)) {
                 attributes.push_back(Attribute(attribute.first));
 //                attributes.emplace_back(attribute.first); // TODO WHAT
@@ -178,25 +207,26 @@ public:
         return child;
     }
 
-    vector<vector<string>> filterInfo(const string &value) {
+    vector<vector<string>> filterInfo(const string &value, vector<vector<string>> data) {
         vector<vector<string>> filteredInfo;
 
-        for (auto &line : info) {
-            if (line[ATTRIBUTES_POSITION[bestAttribute]] == value) {
+        for (auto &line : data) {
+            if (line[ATTRIBUTE_TO_POSITION.at(bestAttribute)]  == value) {
                 filteredInfo.push_back(line);
             }
         }
         return filteredInfo;
     }
 
-    string getBestAttribute() {
+    string calculateBestAttribute() {
         double entropyOfTarget = getEntropyOfTarget();
         double maxGain = INT_MIN;
         string attributeWithMaxGain;
         double gain;
         for (auto &attribute : attributes) {
+            double br = attribute.getEntropy();
             gain = entropyOfTarget - attribute.getEntropy();
-            if (gain - maxGain > 0.000000001) {
+            if (gain > maxGain) {
                 maxGain = gain;
                 attributeWithMaxGain = attribute.getAttributeName();
             }
@@ -207,11 +237,15 @@ public:
     [[nodiscard]] double getEntropyOfTarget() const {
         double recurrenceProbability = (double) recurrenceCount / allAnswers;
         double noRecurrenceProbability = 1.0 - recurrenceProbability;
-        return -recurrenceProbability * log2(recurrenceProbability) -
-               noRecurrenceProbability * log2(noRecurrenceProbability);
+        if (recurrenceProbability == 0 or noRecurrenceProbability == 0) {
+            return 0;
+        } else {
+            return -recurrenceProbability * log2(recurrenceProbability) -
+                   noRecurrenceProbability * log2(noRecurrenceProbability);
+        }
     }
 
-    string getClassWithMoreAnswers() {
+    string getClassWithMoreAnswers() const {
         if (2 * recurrenceCount > allAnswers) {
             return RECURRENCE;
         } else if (2 * recurrenceCount == allAnswers) {
@@ -222,10 +256,17 @@ public:
     }
 
     [[nodiscard]] bool isLeafNode() const {
-        if (recurrenceCount == 0 || recurrenceCount == allAnswers || 2 * recurrenceCount == allAnswers) {
-            return true;
+        return (recurrenceCount == 0 || recurrenceCount == allAnswers || allAnswers < K);
+    }
+
+    string getTargetClass() const {
+        if (recurrenceCount == 0) {
+            return NO_RECURRENCE;
+        } else if (recurrenceCount == allAnswers) {
+            return RECURRENCE;
+        } else {
+            return getClassWithMoreAnswers();
         }
-        return false;
     }
 
     void setBestAttribute(const string &attribute) {
@@ -233,65 +274,99 @@ public:
     }
 
     vector<string> getValues() {
-        return ATTRIBUTES[bestAttribute];
+        return ATTRIBUTE_TO_VALUES.at(bestAttribute);   //ATTRIBUTE_TO_VALUES[bestAttribute];
+    }
+
+    const string &getBestAttribute() const {
+        return bestAttribute;
     }
 };
 
-static vector<string> getFileLines() {
+static vector<string> getFileLines(const string &fileName) {
     string line;
     vector<string> fileLines;
-    std::ifstream myfile(fileName);
-    if (myfile.is_open()) {
-        while (getline(myfile, line)) {
+    std::ifstream myFile(fileName);
+    if (myFile.is_open()) {
+        while (getline(myFile, line)) {
             fileLines.push_back(line);
         }
-        myfile.close();
+        myFile.close();
     } else
         cout << "Unable to open file " << fileName << "\n";
-//    std::random_shuffle(fileLines.begin(), fileLines.end());
+    std::random_shuffle(fileLines.begin(), fileLines.end());
     return fileLines;
 }
 
-class ID3 {
-private:
-    DecisionTreeNode *root;
-    vector<vector<string>> lines; // TODO RENAME
-public:
-    ID3(string fileName) {
-        root = new DecisionTreeNode();
+static vector<vector<string>> getDataInTokens(vector<string> someData) {
+    string delimiter = ",";
+    int position;
+    vector<vector<string>> dataInTokens;
+
+    for (auto &line : someData) {
+        vector<string> token;
+        while ((position = line.find(delimiter)) != std::string::npos) {
+            token.push_back(line.substr(0, position));
+            line.erase(0, position + 1);
+        }
+        token.push_back(line.substr(0, position));
+        dataInTokens.push_back(token);
     }
 
-    void readTrainingSets(vector<string> trainingSet) {
-        string delimiter = ",";
-        int position;
+    return dataInTokens;
+}
 
-        // line: no-recurrence-events,30-39,premeno,30-34,0-2,no,3,left,left_low,no
-        for (auto &line : trainingSet) {
-            vector<string> token;
-            while ((position = line.find(delimiter)) != std::string::npos) {
-                token.push_back(line.substr(0, position)); // TODO
-                line.erase(0, position + 1);
-            }
-            lines.push_back(token);
-        }
-    };
+
+class ID3 {
+private:
+    string fileName;
+//    DecisionTreeNode *root;
+//    vector<vector<string>> allData; // TODO RENAME
+public:
+    explicit ID3(string fileName) : fileName(std::move(fileName)) {}
 
     void buildTree(DecisionTreeNode *currentNode, vector<vector<string>> data) {
-        currentNode->processData(data);
-        string bestAttribute = currentNode->getBestAttribute();
+        currentNode->processData(data); // TODO PROBLEM
+
+        if (!currentNode->isLeafNode()) {
+        string bestAttribute = currentNode->calculateBestAttribute();
         currentNode->setBestAttribute(bestAttribute);
         vector<string> allValues = currentNode->getValues();
-        for (auto& value : allValues){
-            DecisionTreeNode* child = currentNode->createChild(value);
-            vector<vector<string>> filteredData = currentNode->filterInfo(value);
-            buildTree(child, filteredData);
+            for (auto &value : allValues) {
+                DecisionTreeNode *child = currentNode->createChild(value);
+                vector<vector<string>> filteredData = currentNode->filterInfo(value, data);
+                buildTree(child, filteredData);
+            }
         }
+    }
+
+    string testSet(vector<string> line, DecisionTreeNode *currentNode) {
+        if (currentNode->isLeafNode()) {
+            return currentNode->getTargetClass();
+        } else {
+            string attribute = currentNode->getBestAttribute(); // 'age'
+            string value = line[ATTRIBUTE_TO_POSITION[attribute]]; // '60-69'
+            return testSet(line, currentNode->getChild(value));
+        }
+    }
+
+    double getAccuracyOfValidationSet(vector<vector<string>> validationSet, DecisionTreeNode *root) {
+        int correctAnswers = 0;
+        int allAnswers = 0;
+
+        for (auto &line : validationSet) { // line: ["no-recurrence-events", "60-69", "ge40", ..]
+            string result = testSet(line, root);
+            if (line[0] == result) {
+                correctAnswers++;
+            }
+            allAnswers++;
+        }
+        return (double) correctAnswers / (double) allAnswers; // Accuracy
     }
 
     void crossValidation() {
         double accuracy;
         double allAccuracies = 0;
-        vector<string> fileLines = getFileLines();
+        vector<string> fileLines = getFileLines(fileName);
 
         int linesSize = fileLines.size();
         int step = linesSize / NUMBER_OF_SETS;
@@ -302,37 +377,33 @@ public:
             vector<string> endOfTrainingSets(fileLines.begin() + step + toValidate, fileLines.end());
             trainingSet.insert(trainingSet.end(), endOfTrainingSets.begin(), endOfTrainingSets.end());
 
-            readTrainingSets(trainingSet);
-            accuracy = getAccuracyOfValidationSet(validationSet);
+            vector<vector<string>> trainingDataInTokens = getDataInTokens(trainingSet);
+            auto *root = new DecisionTreeNode();
+            buildTree(root, trainingDataInTokens); // TODO
+            vector<vector<string>> validationDataInTokens = getDataInTokens(validationSet);
+            accuracy = getAccuracyOfValidationSet(validationDataInTokens, root);
             allAccuracies += accuracy;
             cout << "Accuracy: " << accuracy << "\n";
-
-            clearVotes();
         }
-        cout << "\nAverage accuracy: " << allAccuracies / NUMBER_OF_SETS << "\n";
+        cout << "\nAverage accuracy: " << (double) allAccuracies / NUMBER_OF_SETS << "\n";
     }
 };
 
 
 int main() {
-//    Age pesho = Age();
-//    pesho.add("10-19");
-//    cout << pesho.getAll();
+    srand(time(nullptr));
 
+    string fileName = "../breast-cancer.data";
+//    ID3 t = ID3(fileName);
+//    vector<string> fileLines = getFileLines(fileName);
+//    vector<vector<string>> data = getDataInTokens(fileLines);
+//    auto *root = new DecisionTreeNode();
+//    t.buildTree(root,data);
+//
+    ID3 tree = ID3(fileName);
+    tree.crossValidation();
 
 //    vector<string> trainingSet {"no-recurrence-events,60-69,ge40,20-24,0-2,no,1,left,left_low,no",
 //            "no-recurrence-events,40-49,premeno,50-54,0-2,no,2,left,left_low,no"};
-//    string delimiter = ",";
-//
-//    int position;
-//    string token;
-//
-//    for (auto &line : trainingSet) {
-//        while ((position = line.find(delimiter)) != std::string::npos) {
-//            token = line.substr(0, position);
-//            cout << token << "\n";
-//            line.erase(0, position + 1);
-//        }
-//    }
     return 0;
 };
